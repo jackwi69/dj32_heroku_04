1ae16e535c4f1d8d047153efb21847f88ecb0f85
jack wi69 <jackwi69@gmail.com>
2022-11-03 15:11:46 +0100

    adesd AbstractUser

--- aplikacja/admin.py
+++ aplikacja/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
 from .models import Article, Publication
 
@@ -9,3 +11,5 @@ admin.site.register(Publication, PublicationAdmin)
 class ArticleAdmin(admin.ModelAdmin):
     pass
 admin.site.register(Article, ArticleAdmin)
+
+admin.site.register(User, UserAdmin)
--- aplikacja/models.py
+++ aplikacja/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    rating = models.PositiveIntegerField(default=settings.DEFAULT_USER_RATING)
 
 class Publication(models.Model):
     title = models.CharField(max_length=30)
